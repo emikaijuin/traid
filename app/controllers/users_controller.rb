34fd114a5cb348474b15590221a1bfe21b13b564
Emika Ijuin <emikaijuin@gmail.com>
2018-03-21 09:11:33 +0000

    Create new user in users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,26 @@
 class UsersController < ApplicationController
-    
+
   def new
+    @user = User.new
   end
   
+  def create
+    if params[:password] == params[:password_confirmation]
+      @user = User.new(user_params)
+      
+      if @user.save
+        flash[:notice] = "Welcome to Traid, #{@user.first_name}!"
+        redirect_to users_path
+      else
+        flash[:notice] = "Sorry, there was an error creating your profile, please try again."
+        render :new
+      end
+    end
+  end
+      
   private 
   
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :username, :birthday, :email, :phone_number, :encrypted_password, :address, :secondary_address, :city, :state, :country)
+    params.require(:user).permit(:first_name, :last_name, :username, :birthday, :email, :phone_number, :password, :password_confirmation, :address, :secondary_address, :city, :state, :country)
   end
 end
