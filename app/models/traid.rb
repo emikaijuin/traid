4c22567370deeb32ad3461d8af9f3543436cef05
Emika Ijuin <emikaijuin@gmail.com>
2018-03-22 07:33:06 +0000

    Add class method to create and return a copy of the traid for the requested user

--- app/models/traid.rb
+++ app/models/traid.rb
@@ -1,22 +1,34 @@
 class Traid < ApplicationRecord
-    belongs_to :user
-    before_save :create_key
+  belongs_to :user
+  before_save :create_key
+  
+  def create_key
+    self.key = SecureRandom.hex(4).upcase unless self.key
+  end
+  
+  class << self
     
-    def create_key
-        self.key = SecureRandom.hex(4).upcase
+    def create_copy(user_id, traid_params, key)
+      @traid_copy = Traid.new(traid_params)
+      @traid_copy.user = User.find(user_id)
+      @traid_copy.key = key
+      @traid_copy
     end
     
-    def self.offer_types
-        ["Goods", "Services"]
+    def offer_types
+      ["Goods", "Services"]
     end
     
-    def self.offer_subtypes
-        # Hash format to be implemented when AJAX is added to form
-        # {
-        #     "Goods" => ["Appliances", "Arts & Crafts", "Books", "Clothes", "Furniture", "Household", "Produce (Homegrown)", "Jewelry", "Tickets", "Toys & Games", "Other"],
-        #     "Services" => ["Automotive", "Beauty", "Cell/Mobille", "Computer", "Creative", "Cycle", "Event", "Farm + Garden", "Financial", "Household", "Labor/Move", "Legal", "Lessons", "Marine", "Pet", "Real Estate", "Skilled Trade", "Travel/Vac", "Write/Ed/Trans", "Other"]
-        # }
-        
-        [ "Appliances", "Arts & Crafts", "Books", "Clothes", "Furniture", "Household", "Produce (Homegrown)", "Jewelry", "Tickets", "Toys & Games", "Automotive", "Beauty", "Cell/Mobille", "Computer", "Creative", "Cycle", "Event", "Farm + Garden", "Financial", "Household", "Labor/Move", "Legal", "Lessons", "Marine", "Pet", "Real Estate", "Skilled Trade", "Travel/Vac", "Write/Ed/Trans", "Other"]
+    def offer_subtypes
+      # Hash format to be implemented when AJAX is added to form
+      # {
+      #     "Goods" => ["Appliances", "Arts & Crafts", "Books", "Clothes", "Furniture", "Household", "Produce (Homegrown)", "Jewelry", "Tickets", "Toys & Games", "Other"],
+      #     "Services" => ["Automotive", "Beauty", "Cell/Mobille", "Computer", "Creative", "Cycle", "Event", "Farm + Garden", "Financial", "Household", "Labor/Move", "Legal", "Lessons", "Marine", "Pet", "Real Estate", "Skilled Trade", "Travel/Vac", "Write/Ed/Trans", "Other"]
+      # }
+      
+      [ "Appliances", "Arts & Crafts", "Books", "Clothes", "Furniture", "Household", "Produce (Homegrown)", "Jewelry", "Tickets", "Toys & Games", "Automotive", "Beauty", "Cell/Mobille", "Computer", "Creative", "Cycle", "Event", "Farm + Garden", "Financial", "Household", "Labor/Move", "Legal", "Lessons", "Marine", "Pet", "Real Estate", "Skilled Trade", "Travel/Vac", "Write/Ed/Trans", "Other"]
     end
+  end
+  
+
 end
