3f1970ec882d7ca2934d1404ab7bb8a7f5358f74
Emika Ijuin <emikaijuin@gmail.com>
2018-03-22 08:45:56 +0000

    Create method which takes status of user traid copies and returns status of overall traid deal

--- app/models/traid.rb
+++ app/models/traid.rb
@@ -2,11 +2,29 @@ class Traid < ApplicationRecord
   belongs_to :user
   before_save :create_key
   
+  enum status: [ :requested, :negotiating, :canceled, :finalized ]
+  
   def create_key
     self.key = SecureRandom.hex(4).upcase unless self.key
   end
   
   class << self
+    
+    def status(status_1, status_2)
+      if status_1 == "requested" || status_2 == "requested"
+        return "Requested"
+      elsif status_1 == nil || status_2 == nil
+        return "Requested"
+      elsif status_1 == "negotiating" || status_2 == "negotiating"
+        return "Negotiating"
+      elsif ( status_1 == "finalized" && status_2 != "finalized" ) || (status_1 != "finalized" && status_2 == "finalized")
+        return "Traid Finalization Pending Approval"
+      elsif status_1 == "canceled" || status_2 == "canceled"
+        return "Canceled"
+      elsif status_1 == "finalized" && status_2 == "finalized"
+        return "Traid Finalized"
+      end
+    end
     
     def create_copy(user_id, traid_params, key)
       @traid_copy = Traid.new(traid_params)
