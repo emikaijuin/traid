e4a8e7cc1a86e70d2973dfd5884d3549ff078026
Emika Ijuin <emikaijuin@gmail.com>
2018-03-22 12:32:55 +0000

    Seed database with 500 users

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,23 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'faker'
+
+ActiveRecord::Base.transaction do
+  500.times do |i|
+    user = User.new 
+    user.first_name = Faker::Name.first_name
+    user.last_name = Faker::Name.last_name
+    user.username = Faker::Internet.user_name
+    user.birthday = Faker::Date.birthday(18,65)
+    user.gender = ["Male","Female"][rand(0..1)]
+    user.email = Faker::Internet.free_email
+    user.phone_number = Faker::PhoneNumber.phone_number
+    user.password = "password"
+    user.address = Faker::Address.street_address
+    user.secondary_address = Faker::Address.secondary_address if i%4 == 0
+    user.city = "Kuala Lumpur"
+    user.state = "Kuala Lumpur"
+    user.country = "Malaysia"
+    user.is_offering = [Faker::Commerce.product_name,Faker::Job.title,Faker::Commerce.product_name]
+    user.is_seeking =  [Faker::Commerce.product_name,Faker::Job.title,Faker::Commerce.product_name]
+    user.save
+  end
+end
