1809ebb68f3244ddb97be4b80d7a09b164bd8d96
Emika Ijuin <emikaijuin@gmail.com>
2018-03-22 03:47:45 +0000

    Create Traids

--- db/migrate/20180322021912_create_traids.rb
+++ db/migrate/20180322021912_create_traids.rb
@@ -1,7 +1,13 @@
 class CreateTraids < ActiveRecord::Migration
   def change
     create_table :traids do |t|
-
+      t.string :title
+      t.string :conditions
+      t.string :offer
+      t.string :offer_type
+      t.string :quantity
+      t.string :key
+      t.int :user_id, null => false, :references => [:users, :id]
       t.timestamps null: false
     end
   end
