d21fc50740c5e699ff80e9123c9e5c2e60f26cdd
Emika Ijuin <emikaijuin@gmail.com>
2018-03-25 10:23:12 +0000

    Add columsn to users to store required information from google

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180323083121) do
+ActiveRecord::Schema.define(version: 20180325092252) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -88,6 +88,11 @@ ActiveRecord::Schema.define(version: 20180323083121) do
     t.string   "is_offering",       array: true
     t.string   "is_seeking",        array: true
     t.string   "about"
+    t.string   "provider"
+    t.string   "uid"
+    t.string   "name"
+    t.string   "oauth_token"
+    t.datetime "oauth_expires_at"
   end
 
   add_foreign_key "authentications", "users"
