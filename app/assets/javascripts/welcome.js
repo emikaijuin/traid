f76beb5b6e7e3983df4630bddeba0727637d3fcc
Emika Ijuin <emikaijuin@gmail.com>
2018-03-26 03:31:50 +0000

    Convert Welcome.coffee to Welcome.js, input JS for Google Maps API

---
+++ app/assets/javascripts/welcome.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+ var mapOptions = {
+    zoom: 14,
+    center: new google.maps.LatLng(37.76, -122.4194),
+    mapTypeId: google.maps.MapTypeId.ROADMAP,
+    draggable: false,
+    disableDefaultUI: true
+
+  }
+  var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+});
